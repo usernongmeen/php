032b21f3bec1d781337be940dce680c4f4c8e64d
usernongmeen <34308392+usernongmeen@users.noreply.github.com>
2017-12-08 02:30:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['id'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
