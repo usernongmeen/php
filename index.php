b0db033e7939fdbff8ba77bd6a95d0ebc8b65870
usernongmeen <34308392+usernongmeen@users.noreply.github.com>
2018-02-21 21:31:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 $access_token = 'Cc62NCw9whOAnXC84ptuVBVHJpE25iKSAVK+96+eHvlmYd9SMvfnLSDImAsAFN47XLqq2YkAvuEPAdNGjmH/vNgPUu3Ej5rMRdr7js6VACSzvj7GWFNIBaFkxTQg8vaqGZ/tFP48mGy1KueQEd4qpgdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -11,26 +10,18 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
+			// Get user profile
+			$profile = $event['source']['userId'];
 			// Get text sent
 			$text = $event['message']['id'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://m3en.myds.me/om/line/line php bot - file upload/get_content.php';
 			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'displayName' => $profile,
+				'messageId' => $text
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -39,9 +30,6 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
-			echo $result . "\r\n";
 		}
 	}
 }
-echo "OK";
